337bf04d0544170d485d8a4617adddde6d586994
JulianRivers <juliancamilorifo@ufps.edu.co>
2026-06-26 09:01:45 -0500

    chore: agregar inicialización de citas-api y notification-service

---
+++ notification-service/src/test/java/com/nelumbo/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelumbo.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
