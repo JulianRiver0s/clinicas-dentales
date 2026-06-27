259a5f6e1945ef38df0f8398f2365eee8c7c0053
JulianRivers <juliancamilorifo@ufps.edu.co>
2026-06-27 09:11:19 -0500

    feat(citas-api): exponer esquema bearer JWT en Swagger

---
+++ citas-api/src/main/java/com/nelumbo/citas_api/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.nelumbo.citas_api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SCHEME = "bearer-jwt";
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes(SCHEME,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SCHEME));
+    }
+}
