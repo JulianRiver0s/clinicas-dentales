7151693603f021a442bd144b0eadcaaa542bc1fd
JulianRivers <juliancamilorifo@ufps.edu.co>
2026-06-27 09:11:18 -0500

    feat(citas-api): cargar par de llaves RSA desde certs con ConfigurationProperties

--- citas-api/src/main/java/com/nelumbo/citas_api/CitasApiApplication.java
+++ citas-api/src/main/java/com/nelumbo/citas_api/CitasApiApplication.java
@@ -2,8 +2,13 @@ package com.nelumbo.citas_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.nelumbo.citas_api.config.RsaKeysConfig;
 
 @SpringBootApplication
+// Habilita la carga de propiedades de claves RSA
+@EnableConfigurationProperties(RsaKeysConfig.class)
 public class CitasApiApplication {
 
 	public static void main(String[] args) {
